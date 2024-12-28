c479f95d16e79360666a5f284ba5afc921dc0238
Adrian <adriansanduta@gmail.com>
2024-12-28 13:57:34 -0600

    project 1.8

--- books.js
+++ books.js
@@ -47,6 +47,15 @@ async function renderBooks(filter) {
   booksWrapper.innerHTML = booksHtml;
 }
 
+function priceHTML(originalPrice, salePrice) {
+  if (!salePrice) {
+    return `$${originalPrice.toFixed(2)}`;
+  }
+  return `<span class="book__price--normal">$${originalPrice.toFixed(
+    2
+  )}</span>$${salePrice.toFixed(2)}`;
+}
+
 function filterBooks(event) {
     renderBooks(event.target.value);
 }
